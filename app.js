30dae74c433538b7b6b3c64dd05288296c552a04
Nishant-coder-bit <nishantanindian@gmail.com>
2021-11-13 11:07:42 +0530

    added mongodb atlas to application

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://admin-nishant:testdb@cluster0.b5ulg.mongodb.net/todolistDB", {useNewUrlParser: true});
 
 const itemsSchema = {
   name: String
